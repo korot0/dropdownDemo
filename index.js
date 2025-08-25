8f935148cd317f0d22ac52676b79de3e217b909b
korot0 <108902180+korot0@users.noreply.github.com>
2025-08-25 00:06:46 -0500

    Add dropdown show/hide functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const addDropdown = (btnId) => {
+const addDropdown = (btnId, dropdownContainer) => {
   const btn = document.querySelector(`#${btnId}`);
+  const dropdown = document.querySelector(`.${dropdownContainer}`);
 
   btn.addEventListener("click", () => {
-    console.log("clicked");
+    dropdown.classList.toggle("hidden");
+  });
+
+  dropdown.addEventListener("mouseleave", () => {
+    dropdown.classList.toggle("hidden");
   });
 };
 
-addDropdown("more-vert-btn");
+addDropdown("more-vert-btn", "dropdown-container");
