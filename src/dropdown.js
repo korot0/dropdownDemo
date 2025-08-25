4a0fa0e2d77c1a3b0d0d70eb396c4b93f8591966
korot0 <108902180+korot0@users.noreply.github.com>
2025-08-25 17:40:31 -0500

    Style dropdown menu

--- src/dropdown.js
+++ src/dropdown.js
@@ -3,23 +3,23 @@ export function initDropdowns() {
     const btn = container.querySelector(".dropdown-btn");
     const dropdown = container.querySelector(".dropdown");
     dropdown.classList.add("hidden");
-    attachListeners(btn, dropdown);
+    attachListeners(container, btn, dropdown);
   });
 }
 
-const attachListeners = (btn, dropdown) => {
-  attachBtnListener(btn, dropdown);
-  attachDropdownListener(dropdown);
+const attachListeners = (container, btn, dropdown) => {
+  attachClickListener(btn, dropdown);
+  attachMouseLeaveListener(container, dropdown);
 };
 
-const attachBtnListener = (btn, dropdown) => {
+const attachClickListener = (btn, dropdown) => {
   btn.addEventListener("click", () => {
     dropdown.classList.toggle("hidden");
   });
 };
 
-const attachDropdownListener = (dropdown) => {
-  dropdown.addEventListener("mouseleave", () => {
-    dropdown.classList.toggle("hidden");
+const attachMouseLeaveListener = (container, dropdown) => {
+  container.addEventListener("mouseleave", () => {
+    dropdown.classList.add("hidden");
   });
 };
